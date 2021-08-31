d74e6f63e7c2c2ad540e0ac7812d3239ca53863d
LoTwT <709937065@qq.com>
2021-08-31 19:23:16 +0800

    feat(navmenu): navMenu

--- src/components/nav-menu/src/NavMenu.vue
+++ src/components/nav-menu/src/NavMenu.vue
@@ -4,17 +4,33 @@
       <img src="@/assets/img/logo.svg" alt="logo" class="img" />
       <span class="logo-title">CMS</span>
     </div>
-    <el-menu default-active="2" class="el-menu-vertical">
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical"
+      background-color="#0c2135"
+      text-color="#b7bdc3"
+      active-text-color="#0a60bd"
+    >
       <template v-for="menu in userMenus" :key="menu.id">
+        <!-- 二级菜单 -->
         <template v-if="menu.type === 1">
-          <el-submenu>
-            <i v-if="menu.icon" :class="menu.icon"></i>
-          </el-submenu>
-          <template v-for="submenu in menu.children" :key="submenu.id">
-          </template>
+          <!-- 二级菜单的可以展开的标题 -->
+          <el-sub-menu :index="menu.id.toString()">
+            <template #title>
+              <i v-if="menu.icon" :class="menu.icon"></i>
+              <span>{{ menu.name }}</span>
+            </template>
+            <!-- 遍历里面的 submenu -->
+            <template v-for="submenu in menu.children" :key="submenu.id">
+              <el-menu-item :index="submenu.id.toString()">
+                <i v-if="submenu.icon" :class="submenu.icon"></i>
+                <span>{{ submenu.name }}</span>
+              </el-menu-item>
+            </template>
+          </el-sub-menu>
         </template>
         <template v-else-if="menu.type === 2">
-          <el-menu-item>
+          <el-menu-item :index="menu.id.toString()">
             <i v-if="menu.icon" :class="menu.icon"></i>
             <span>{{ menu.name }}</span>
           </el-menu-item>
@@ -44,17 +60,50 @@ const userMenus = computed(() => store.state.login.userMenus)
     flex-direction: row;
     justify-content: flex-start;
     align-items: center;
+
+    .img {
+      height: 100%;
+      margin: 0 10px;
+    }
+
+    .logo-title {
+      font-size: 16px;
+      font-weight: 700;
+      color: white;
+    }
   }
 
-  .img {
-    height: 100%;
-    margin: 0 10px;
+  .el-menu {
+    border-right: none;
   }
 
-  .logo-title {
-    font-size: 16px;
-    font-weight: 700;
-    color: white;
+  // 目录
+  .el-submenu {
+    background-color: #001529 !important;
+    // 二级菜单 ( 默认背景 )
+    .el-menu-item {
+      padding-left: 50px !important;
+      background-color: #0c2135 !important;
+    }
   }
+
+  ::v-deep .el-submenu__title {
+    background-color: #001529 !important;
+  }
+
+  // hover 高亮
+  .el-menu-item:hover {
+    color: #fff !important; // 菜单
+  }
+
+  .el-menu-item.is-active {
+    color: #fff !important;
+    background-color: #0a60bd !important;
+  }
+}
+
+.el-menu-vertical:not(.el-menu--collapse) {
+  width: 100%;
+  height: calc(100% - 48px);
 }
 </style>
