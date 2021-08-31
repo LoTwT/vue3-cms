45cca5c88cb486c5f8b0e546358a87b7b9d116b6
LoTwT <709937065@qq.com>
2021-08-31 17:47:18 +0800

    chore(.env.development): vITE_API_PROXY_URL

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 a cms project...
 
+## config
+
+`yarn dev` 时如需使用 proxy，请配置 `.env.development` 文件内的 `VITE_API_PROXY_URL` :smile:
+
 ## errors
 
 `LoginPanel.vue` 的问题为：`<script setup lang="ts">` 内使用 `defineExpose` 暴露组件内属性时，调用方使用 `InstanceType<typeof LoginPanel>` 时，没有将被暴露属性的类型声明添加到组件实例的类型声明内。详见 [vue3 issue #4397](https://github.com/vuejs/vue-next/issues/4397)
--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,36 @@
-import { defineConfig } from "vite"
+import { ConfigEnv, loadEnv, UserConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import VitePluginElementPlus from "vite-plugin-element-plus"
 import path from "path"
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VitePluginElementPlus({
-      // 如果你需要使用 [component name].scss 源文件，你需要把下面的注释取消掉。
-      // 对于所有的 API 你可以参考 https://github.com/element-plus/vite-plugin-element-plus
-      // 的文档注释
-      // useSource: true
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://152.136.185.210:4000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+  const { VITE_API_PROXY_URL } = env
+
+  return {
+    plugins: [
+      vue(),
+      VitePluginElementPlus({
+        // 如果你需要使用 [component name].scss 源文件，你需要把下面的注释取消掉。
+        // 对于所有的 API 你可以参考 https://github.com/element-plus/vite-plugin-element-plus
+        // 的文档注释
+        // useSource: true
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
-})
+    server: {
+      proxy: {
+        "/api": {
+          target: VITE_API_PROXY_URL ?? "",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  }
+}
