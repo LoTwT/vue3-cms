5e3b4d7b0d8982b392e71edf63079d044116b478
LoTwT <709937065@qq.com>
2021-09-05 16:03:58 +0800

    fix(main.ts): deal the problem that push to "not-found" after refresh

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,12 @@ import "element-plus/dist/index.css"
 import { customGlobalConfig } from "./global"
 
 const app = createApp(App)
-app.use(router).use(store).use(customGlobalConfig)
 
+// setupStore() 根据请求数据注册了路由
+// 需要在注册 router 之前调用，才能够正确匹配刷新时的跳转 url
 setupStore()
+app.use(router)
+app.use(store)
+app.use(customGlobalConfig)
 
 app.mount("#app")
