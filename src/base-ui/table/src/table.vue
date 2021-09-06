a6a7aaa3fb2cd146247c011e8ce9440a5a4f8d6b
LoTwT <709937065@qq.com>
2021-09-06 23:39:47 +0800

    feat(cms-table.vue): scope slot

--- src/base-ui/table/src/table.vue
+++ src/base-ui/table/src/table.vue
@@ -6,7 +6,13 @@ defineProps<{ dataList: any[]; propList: any[] }>()
   <div class="cms-table">
     <el-table :data="dataList" border style="width: 100%">
       <template v-for="propItem in propList" :key="propItem.prop">
-        <el-table-column v-bind="propItem" align="center"></el-table-column>
+        <el-table-column v-bind="propItem" align="center">
+          <template #default="scope">
+            <slot :name="propItem.slotName" :row="scope.row">
+              {{ scope.row[propItem.prop] }}
+            </slot>
+          </template>
+        </el-table-column>
       </template>
     </el-table>
   </div>
--- src/views/main/system/user/User.vue
+++ src/views/main/system/user/User.vue
@@ -21,9 +21,19 @@ const propList = [
   { prop: "name", label: "用户名", minWidth: "100" },
   { prop: "realname", label: "真实姓名", minWidth: "100" },
   { prop: "cellphone", label: "手机号码", minWidth: "100" },
-  { prop: "enable", label: "状态", minWidth: "100" },
-  { prop: "createAt", label: "创建时间", minWidth: "250" },
-  { prop: "updateAt", label: "更新时间", minWidth: "250" },
+  { prop: "enable", label: "状态", minWidth: "100", slotName: "status" },
+  {
+    prop: "createAt",
+    label: "创建时间",
+    minWidth: "250",
+    slotName: "createAt",
+  },
+  {
+    prop: "updateAt",
+    label: "更新时间",
+    minWidth: "250",
+    slotName: "updateAt",
+  },
 ]
 </script>
 
@@ -31,7 +41,17 @@ const propList = [
   <div class="user">
     <page-search :search-form-config="searchFormConfig" />
     <div class="content">
-      <cms-table :data-list="userList" :prop-list="propList" />
+      <cms-table :data-list="userList" :prop-list="propList">
+        <template #status="scope">
+          <el-button>{{ scope.row.enable ? "启用" : "禁用" }}</el-button>
+        </template>
+        <template #createAt="scope">
+          <strong>{{ scope.row.createAt }}</strong>
+        </template>
+        <template #updateAt="scope">
+          <strong>{{ scope.row.updateAt }}</strong>
+        </template>
+      </cms-table>
     </div>
   </div>
 </template>
